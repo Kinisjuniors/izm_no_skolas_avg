df46c09bfee6c7eff2e78213b842ef199a531495
Ernests Ēvers01~ <kinigadners598@gmail.com>
2023-12-07 17:26:37 +0200

    Pirmais pabeigts_counter

---
+++ app.js
@@ -0,0 +1,29 @@
+let counter = 0;
+
+let buttons = document.querySelectorAll("button");
+let value = document.getElementById("number");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(button.innerText == "Decrease") {
+            counter--;
+        }
+        else if(button.innerText == "Increase") {
+            counter++;
+        } else {
+            counter=0;
+        }
+
+        if (counter >0) {
+            value.style.color = "green"
+        } else if (counter <0) {
+            value.style.color = "red"
+        } else if (counter ==0) {
+            value.style.color = "black"
+        }
+
+        
+        value.innerText = counter;
+    }
+    )
+})
